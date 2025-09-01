606cd3ca6285bafdb00e0bb0d19b99563089418e
Mubashir Shahzaib <mobishahzaib@gmail.com>
2025-09-02 00:50:34 +0500

    Add project files.

---
+++ First Program/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace First_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, Mubashir Shahzaib!");
+            Console.WriteLine("It seems like you will need more RAM!");
+
+        }
+    }
+}
